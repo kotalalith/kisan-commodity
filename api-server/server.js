6d5b9c51959fb121b2dce536f4a96b00c72e23c0
Kotalalith <kotaadityagupta@gmail.com>
2026-05-26 20:58:50 +0530

    firebase env auth fix

--- api-server/server.js
+++ api-server/server.js
@@ -12,7 +12,13 @@ app.use(express.json());
 // Initialize Firebase Admin SDK
 let db;
 try {
-  const serviceAccount = require('./serviceAccountKey.json');
+  let serviceAccount;
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } else {
+    serviceAccount = require('./serviceAccountKey.json');
+  }
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
